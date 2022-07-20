52ce5801e67651d5553dff42fb3f7783ed95add7
KOMILJONOV <komiljonovshukurullox@gmail.com>
2022-07-20 11:08:19 +0500

    dfg

--- app/urls.py
+++ app/urls.py
@@ -3,14 +3,36 @@ from django.urls import path
 from .models import New
 
 def news(request):
+    news: list[New] = New.objects.all()
     return JsonResponse(
         {
             'data': [
-                new.json for new in New.objects.all()
+                new.json for new in news
             ]
         }
     )
 
+
+def new_one(request, pk):
+    print(pk, type(pk))
+    new:New = New.objects.filter(id=pk).first()
+    print(new)
+    if new:
+        return JsonResponse(
+            {
+                "ok": True,
+                "data": new.json
+            }
+        )
+    else:
+        return JsonResponse(
+            {
+                "ok": False,
+                "error": 'not_found'
+            }
+        )
+
 urlpatterns = [
-    path('news', news)
+    path('news', news),
+    path('new/<int:pk>', new_one)
 ]
